b32cf449d372aabe7498f1491b081cc709c52239
Robarrot <131776452+Robarrot@users.noreply.github.com>
2023-12-01 21:11:48 +0300

    Add files via upload

---
+++ votingprocess.py
@@ -0,0 +1,15 @@
+# votingProcess.py
+
+import tkinter as tk
+from tkinter import *
+from PIL import ImageTk, Image
+
+def voteCast(root, frame1, vote, client_socket):
+    # Code for casting a vote
+
+def votingPg(root, frame1, client_socket):
+    # Code to display the voting interface
+
+# other functions related to the voting process
+
+
